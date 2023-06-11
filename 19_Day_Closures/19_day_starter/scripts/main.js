477307b967983adfc67e80595ac05771fc30407a
jingrenlim <lim-jr@hotmail.com>
2023-06-11 21:31:52 +0800

    added some to Promises and done with day 19

--- 19_Day_Closures/19_day_starter/scripts/main.js
+++ 19_Day_Closures/19_day_starter/scripts/main.js
@@ -1,2 +1,40 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// Closures are just inner functions that access the variables
+// of the outer function. Kinda like python decorators.
+// The usefulness of closures is that closures can store state.
+// Basically, it is because functions are first class objects.
+
+function outerFunction() {
+  let count = 0;
+  function innerFunction() {
+    count++;
+    return count;
+  }
+  return innerFunction;
+}
+const innerFunc = outerFunction();
+// you can call this repeatedly and then the count will increase
+console.log(innerFunc());
+console.log(innerFunc());
+console.log(innerFunc());
+
+// You can be creative and return an obj
+function outerFunction1() {
+  let count = 0;
+  function plusOne() {
+    count++;
+    return count;
+  }
+  function minusOne() {
+    count--;
+    return count;
+  }
+  // you can return an objects that attributes the inner functions
+  return {
+    plusone: plusOne(),
+    minusOne: minusOne(),
+  };
+}
+const innerFunc2 = outerFunction1();
+// then you can call them like this to access each inner function
+console.log(innerFunc2.plusone);
+console.log(innerFunc2.minusOne);
